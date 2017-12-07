126819b0600d15f6fddfac345d6c35d0a8c207ea
Marly <marlycormar@gmail.com>
2017-12-07 14:59:16 -0500

    Include imagemap hook as a module

--- ExternalModule.php
+++ ExternalModule.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * @file
- * Provides ExternalModule class for Auto Populate Fields.
+ * Provides ExternalModule class for Pain Map.
  */
 
 namespace PainMap\ExternalModule;
 
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
-
+use Form;
 
 /**
- * ExternalModule class for Auto Populate Fields.
+ * ExternalModule class for Pain Map.
  */
 class ExternalModule extends AbstractExternalModule {
 
@@ -19,36 +19,104 @@ class ExternalModule extends AbstractExternalModule {
      * @inheritdoc
      */
     function hook_every_page_top($project_id) {
-        $this->addImagemapActionTag($project_id);
-    }
-    
-    
-    /**
-     * Adds @IMAGEMAP as an action tag.
-     *
-     * @param int $project_id
-     *   An integer containing the current project id.
-     */
-    protected function addImagemapActionTag($project_id) {
-        $js_files = array();
-
         if (PAGE == 'Design/online_designer.php' && $project_id) {
-            $js_files[] = 'js/helper.js';
+            $this->includeJs('js/helper.js');
         }
+
+        if (!in_array(PAGE, array('DataEntry/index.php', 'surveys/index.php', 'Surveys/theme_view.php'))) {
+            return;
+        }
+
+        if (empty($_GET['id'])) {
+            return;
+        }
+
+        // Checking additional conditions for survey pages.
+        if (PAGE == 'surveys/index.php' && !(isset($_GET['s']) && defined('NOAUTH'))) {
+            return;
+        }
+
+        global $Proj;
+        $settings = array();
+
+        foreach (array_keys($Proj->forms[$_GET['page']]['fields']) as $field_name) {
+            $field_info = $Proj->metadata[$field_name];
+            
+
+            if (!$display_mode = Form::getValueInActionTag($field_info['misc'], '@IMAGEMAP')) {
+                continue;
+            }
+
+            //print_r($field_info);
+            
+            $row = $this->getDefaultConfig($display_mode);
+            $row['field'] = $field_name;
+
+            $b64 = base64_encode(file_get_contents($this->getUrl($row['image'])));
+            $src = "data:image/png;base64,$b64";
+            $row['src'] = $src;
+
+            $row['areas'] = file_get_contents($this->getUrl($row['map']));
+            $row['type'] = $field_info['element_type'];
+
+            $settings[] = $row;
+        }
+
+        if (empty($settings)) {
+            return;
+        }
+
+        echo '<script>var imageMapLibrary = ' . json_encode($settings) . ';</script>';
         
-        $this->loadJsFiles($js_files);
+        $this->includeJs('js/imageMapster.js');
+        $this->includeJs('js/imagemap.js');
+        
+        if(!empty($Proj->metadata['painmap_female']))
+            print_r ($Proj->metadata['painmap_female']);
+
     }
-    
-    
+
     /**
-     * Loads js files.
+     * Includes a local JS file.
      *
-     * @param array $js_files
-     *   An array of js files paths within the module.
+     * @param string $path
+     *   The relative path to the js file.
      */
-    protected function loadJsFiles($js_files) {
-        foreach ($js_files as $file) {
-            echo '<script src="' . $this->getUrl($file) . '"></script>';
+    protected function includeJs($path) {
+        echo '<script src="' . $this->getUrl($path) . '"></script>';
+    }
+
+    protected function getDefaultConfig($display_mode) {
+        switch ($display_mode) {
+            case 'PAINMAP_MALE':
+                return array(
+                    'name' => 'painmap_male',
+                    'alt' => 'Male Front Pain Map',
+                    'image' => 'img/painmap_male.png',
+                    'width' => 553,
+                    'height'=> 580,
+                    'map' => 'maps/painmap_male.html'
+                );
+            case 'PAINMAP_FEMALE':
+                return array(
+                    'name'  => 'painmap_female',
+                    'alt'   => "Female Front Pain Map",
+                    'image' => "img/painmap_female.png",
+                    'width' => 518,
+                    'height'=> 580,
+                    'map'   => "maps/painmap_female.html"
+                );
+            case  'SMILE_SCALE':
+                return array(
+                    'name'  => 'smile_scale',
+                    'alt'   => "Smile Scale",
+                    'image' => "img/smile_scale.png",
+                    'width' => 602,
+                    'height'=> 147,
+                    'map'   => "maps/smile_scale.html",
+                    'singleSelect' => true,
+                    'fillColor'    => '00aa00'
+                );
         }
     }
 }
